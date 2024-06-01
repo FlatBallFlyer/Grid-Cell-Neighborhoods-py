6bd8415a7ae5d374e64a454a2aa8fe44a7d8d37a
Mike Storey <mike@agile-learning.institute>
2024-06-01 10:40:36 -0400

    Koan Created

---
+++ community.py
@@ -0,0 +1,12 @@
+class Community:
+  
+  def __init__(self, matrix, distance):
+    # TODO: Initilize Community
+    return
+
+  def getCount(self):
+    """
+    Get the number of members in the community.
+    """
+    # TODO: Count Members
+    return 0
